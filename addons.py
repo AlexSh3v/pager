7cbebcaf18241d91d65393117b2e497a18a3a646
AlexSh3v (Ubuntu) <aleksei567gi@gmail.com>
2024-07-05 15:09:53 +0300

    added time reminder and emojies

--- addons.py
+++ addons.py
@@ -1,7 +1,9 @@
 import dataclasses
+import datetime
 import json
 import pathlib
 import random
+import time
 
 extra_dir = pathlib.Path(__file__).parent / 'extra'
 
@@ -25,11 +27,36 @@ class ProgrammingJoke:
         return (question_index < dot_index) or (question_index > 0 and dot_index == -1)
 
 
+@dataclasses.dataclass
+class TimeReminder:
+    min_sec: int
+    max_sec: int
+    current: int = 0
+    started: float = 0.
+
+    def __post_init__(self):
+        self.new()
+
+    def new(self) -> int:
+        self.started = time.perf_counter()
+        self.current = random.randint(self.min_sec, self.max_sec)
+        return self.current
+
+    def is_over(self) -> bool:
+        return time.perf_counter()-self.started >= self.current
+
+    @staticmethod
+    def get_current_time() -> str:
+        now = datetime.datetime.now()
+        return now.strftime("Now is %B %d %Y - %H:%M:%S")
+
+
 def get_random_motivational_quote() -> MotivationalQuote:
     with motivational_quotes_path.open() as json_file:
         quotes = json.load(json_file)
     data = random.choice(quotes)
-    return MotivationalQuote(data['text'], data['from'])
+    emoji = random.choice('⚡️🔥🌟💪')
+    return MotivationalQuote(data['text'] + ' ' + emoji, data['from'])
 
 
 def get_random_programming_joke() -> str:
@@ -38,8 +65,8 @@ def get_random_programming_joke() -> str:
     data = random.choice(jokes)
     joke = ProgrammingJoke(data['joke'])
     if joke.is_start_with_question():
-        return 'Wait! Do you know ' + joke.text[0].lower() + joke.text[1:]
-    return joke.text
+        return 'Wait! Do you know ' + joke.text[0].lower() + joke.text[1:] + ' ' + '😂'
+    return joke.text + ' 😂'
 
 
 functions = [
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import types
+import typing
 
 import bext
 import time
@@ -9,13 +11,21 @@ import addons
 def main():
     bext.hide_cursor()
     bext.clear()
+    time_reminder = addons.TimeReminder(*TIME_REMINDER_RANGE_SECONDS)
     while True:
+
+        if time_reminder.is_over():
+            render(time_reminder.get_current_time)
+            time_reminder.new()
+            continue
+
         picked_function = random.choice(addons.functions)
         if picked_function == addons.get_random_motivational_quote:
             quote = picked_function()
             render(f'"{quote.quote}" — {quote.author}')
         elif picked_function == addons.get_random_programming_joke:
             render(picked_function())
+
 
 def demo():
     text = "Hello, World! 🌎"
@@ -26,25 +36,35 @@ def demo():
     bext.show_cursor()
 
 
-def scroll_from_beyond_right_to_beyond_left(text, width):
-    text = text.strip() + ' '  # hack
-    left_edge_x = width
-    right_x = -len(text)
+def scroll_from_beyond_right_to_beyond_left(text: str | typing.Callable[[], str], width: int):
 
-    for i in range(len(text) + width):
-        bext.goto(0, 0)
+    def get_text():
+        t = text
+        if isinstance(text, typing.Callable):
+            t = text()
+        return t.strip() + ' '  # hack
+
+    left_edge_x = width
+    right_x = -len(get_text())
+    height = bext.height() // 2
+
+    for i in range(len(get_text()) + width):
+        bext.goto(0, height)
 
         left_space = ' ' * max(left_edge_x - i, 0)
         right_space = ' ' * max(right_x, 0)
         start_index = max(-left_edge_x + i, 0)
 
-        s = left_space + text[start_index:i] + right_space
+        s = left_space + get_text()[start_index:i] + right_space
         print(s)
+
+        bext.goto(0, height+1)
+        print(' ' * width)
 
         time.sleep(DELAY_SECONDS)
 
 
-def render(s: str):
+def render(s: str | typing.Callable[[], str]):
     scroll_from_beyond_right_to_beyond_left(s, bext.width())
     # scroll_till_edge(s, bext.width())
     # scroll_beyond_edge(s, bext.width())
@@ -52,6 +72,7 @@ def render(s: str):
 
 
 DELAY_SECONDS = 0.1
+TIME_REMINDER_RANGE_SECONDS = (60, 80)
 
 
 if __name__ == '__main__':
